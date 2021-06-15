1ecbb6380780dc08fe0eece2912c6c3d94e9b386
Yongjun <using@dimigo.hs.kr>
2021-06-15 22:29:47 +0900

    feat: open another activity on button click

---
+++ app/src/main/java/com/implude/tutorial/intent/AnotherActivity.java
@@ -0,0 +1,3 @@
+package com.implude.tutorial.intent;public class AnotherActivity {
+
+}
--- app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
+++ app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
@@ -1,11 +1,22 @@
+
 package com.implude.tutorial.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_open=findViewById<Button>(R.id.button)
+
+        btn_open.setOnClickListener{
+            val it = (Intent(this, AnotherActivity::class.java))
+            startActivity(it)
+        }
     }
+
 }
